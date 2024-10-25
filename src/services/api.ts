40128372f40c7ac9d346a0e530ee5b3474af4230
aar-rafi <ssrabonislam2000@gmail.com>
2024-10-25 09:26:33 +0600

    ok

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,18 +1,18 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: import.meta.env.VITE_TRAIN_API_URL || 'http://localhost:3000/api',
-  headers: {
-    'Content-Type': 'text/plain',
-  },
-});
+// const api = axios.create({
+//   baseURL: import.meta.env.VITE_TRAIN_API_URL || 'http://localhost:3000/api',
+//   headers: {
+//     'Content-Type': 'text/plain',
+//   },
+// });
 
 //send to irtiaz 
 export const trainService = {
   searchTrains: async (params: { from_station_name: string; to_station_name: string; date: string }) => {
     console.log('searchTrains', params);
     try {
-      const response = await api.post('/api/trains/get', params );
+      const response = await axios.post(`${import.meta.env.VITE_TRAIN_API_URL}/api/trains/get`, params );
       console.log('response_train', response.data);
       // const { date, ...paramsWithoutDate } = params;
       // console.log('pr', paramsWithoutDate);
@@ -21,7 +21,7 @@ export const trainService = {
         to_station_name:params.to_station_name
       }
       console.log('pr', paramsWithoutDate);
-      const r_price = await api.post(`${import.meta.env.VITE_PRICE_API_URL}/api/price/stationids`,  paramsWithoutDate );
+      const r_price = await axios.post(`${import.meta.env.VITE_PRICE_API_URL}/api/price/stationids`,  paramsWithoutDate );
       
 
       return {...response.data, price: r_price.data.price};
@@ -33,7 +33,7 @@ export const trainService = {
 
   getAvailableSeats: async (trainId: string, date: string) => {
     try {
-      const response = await api.get(`/trains/${trainId}/seats`, {
+      const response = await axios.get(`{import.meta.env.VITE_TRAIN_API_URL}/api/trains/${trainId}/seats`, {
         params: { date },
       });
       return response.data;
@@ -45,7 +45,7 @@ export const trainService = {
 
   bookSeats: async (trainId: string, seats: string[], date: string) => {
     try {
-      const response = await api.post(`/trains/${trainId}/book`, {
+      const response = await axios.post(`${import.meta.env.VITE_TRAIN_API_URL}/api/trains/${trainId}/book`, {
         seats,
         date,
       });
